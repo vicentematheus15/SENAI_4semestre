3ec56d7483d9831f17dff6c6854621be16d8e6c0
vicentematheus_15 <vicentematheus1507@gmail.com>
2026-08-11 21:54:43 -0300

    feat: add initial server setup with dotenv and express

---
+++ rede_social_atividade_fisica/src/app.js
@@ -0,0 +1,11 @@
+import 'dotenv/config';
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(3000, () => {
+    console.log("Servidor rodando em: http://localhost:3000")
+}
+)
