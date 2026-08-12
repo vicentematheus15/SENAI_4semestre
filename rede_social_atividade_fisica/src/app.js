8d491ad959ef0aed7415656e50784daea740d9cc
vicentematheus_15 <vicentematheus1507@gmail.com>
2026-08-11 22:05:47 -0300

    feat: add routes for usuarios and atividades

--- rede_social_atividade_fisica/src/app.js
+++ rede_social_atividade_fisica/src/app.js
@@ -5,6 +5,9 @@ const app = express();
 
 app.use(express.json());
 
+app.use('/usuarios', usuariosRoutes);
+app.use('/atividades', atividadesRoutes);
+
 app.listen(3000, () => {
     console.log("Servidor rodando em: http://localhost:3000")
 }
