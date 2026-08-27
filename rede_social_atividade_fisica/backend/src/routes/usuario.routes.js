949f08c9c6d5c275c459864a24eded16480df44a
vicentematheus15 <vicentematheus1507@gmail.com>
2026-08-27 18:35:06 -0300

    fix: update usuarioRoutes to enable login functionality

--- rede_social_atividade_fisica/backend/src/routes/usuario.routes.js
+++ rede_social_atividade_fisica/backend/src/routes/usuario.routes.js
@@ -3,8 +3,7 @@ import * as controller from "../controllers/usuario.controller.js"
 
 const usuarioRoutes = Router();
 
-usuarioRoutes.post('/cadastrar', controller.cadastrar);
-//usuarioRoutes.post('/login', controller.login);
+usuarioRoutes.post('/login', controller.login);
 
 
 export default usuarioRoutes;
