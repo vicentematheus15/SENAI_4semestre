9d356f3f1c62ad58cb1f7265d1e06883e0ede541
vicentematheus15 <vicentematheus1507@gmail.com>
2026-08-27 18:30:35 -0300

    feat: implement login functionality with email and password validation

--- rede_social_atividade_fisica/backend/src/controllers/usuario.controller.js
+++ rede_social_atividade_fisica/backend/src/controllers/usuario.controller.js
@@ -2,7 +2,25 @@ import { Usuario, Atividade } from "../models/index.js";
 
 export async function login(req, res){
     try {
-        {}
+        const {email, senha} = req.body;
+        
+        if(!email || !senha){
+            return res.status(400).json({ erro: "Email ou senha obrigatório" });
+        }
+
+        const usuario = await Usuario.findOne({ where: { email }});
+
+        if(!usuario || usuario.senha !== senha){
+            return res.status(401).json({ erro: "Email ou senha incorretos" });
+        }
+
+        return res.status(200).json({
+            usuarioId: usuario.usuarioId,
+            nome: usuario.nome,
+            nome_usuario: usuario.nome_usuario,
+            imagem: usuario.imagem
+        });
+
     } catch (error) {
         console.error(error)
         return res.status(500).json({erro: "Erro interno do servidor!"})
