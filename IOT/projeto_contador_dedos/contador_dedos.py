5ea7782a291e8eb60357b9d153bbd509106a9c3a
vicentematheus15 <vicentematheus1507@gmail.com>
2026-08-26 21:07:31 -0300

    fix: update camera capture comment for clarity

--- IOT/projeto_contador_dedos/contador_dedos.py
+++ IOT/projeto_contador_dedos/contador_dedos.py
@@ -22,7 +22,7 @@ HAND_CONNECTIONS = [
 ]
 
 # Tentar abrir câmera
-video = cv2.VideoCapture("contador_dedos.mov")
+video = cv2.VideoCapture("contador_dedos.mov") #se trocar o valor para 0, pega da câmera que estiver conectada no usb do pc
 if not video.isOpened():
     video = cv2.VideoCapture(1)
 if not video.isOpened():
