ddeb77a3b82de8f2b331274def9b61b4e42e8624
vicentematheus15 <vicentematheus1507@gmail.com>
2026-08-27 21:27:41 -0300

    fix: update atividadeRoutes to correct method for listarAtividades and ensure proper route structure

--- rede_social_atividade_fisica/backend/src/routes/atividade.routes.js
+++ rede_social_atividade_fisica/backend/src/routes/atividade.routes.js
@@ -3,7 +3,10 @@ import * as controller from '../controllers/atividade.controller.js';
 
 const atividadeRoutes = Router();
 
-atividadeRoutes.post('/listar', controller.listarAtividades)
+atividadeRoutes.get('/', controller.listarAtividades);
+atividadeRoutes.post('/criar', controller.criarAtividade);
+atividadeRoutes.post('/:atividadeId/curtir', controller.curtirAtividade);
+atividadeRoutes.post('/:atividadeId/comentar', controller.comentarAtividade);
 
 
 export default atividadeRoutes;
