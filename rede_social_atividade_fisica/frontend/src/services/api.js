019c80c42268ad7c0d6372047df1cdf67f5dd459
vicentematheus15 <vicentematheus1507@gmail.com>
2026-09-09 21:41:48 -0300

    feat: update API service to use environment variable for base URL and add activity-related functions

--- rede_social_atividade_fisica/frontend/src/services/api.js
+++ rede_social_atividade_fisica/frontend/src/services/api.js
@@ -1,5 +1,34 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseUrl: 'http://localhost:3000'
+    baseUrl: import.meta.env.VITE_API_URL
 });
+
+export async function login(email, senha) {
+    const response = await api.post('usuaros/login', {email, senha});
+    return response.data;
+};
+
+export async function listarAtividades(tipo, page, usuarioId) {
+    const response = await api.get('/atividades',{
+        params: { tipo, page, usuarioId }
+    });
+    return response.data;
+};
+
+export async function criarAtividade(dados){
+    const response = await api.post('/atividades/criar', dados);
+    return response.data;
+};
+
+export async function curtirAtividade(atividadeId, usuarioId){
+    const response = await api.post(`/atividades/${atividadeId}/curtir`, { usuarioId });
+    return response.data;
+};
+
+export async function comentarAtividade(atividadeId, usuarioId){
+    const response = await api.post(`/atividades/${atividadeId}/comentar`, { usuarioId });
+    return response.data;
+};
+
+export default api;
